5473c532b0a186c90133e74ae764c9e401796ffa
Dat Luong <unlimited2989@gmail.com>
2022-09-21 17:23:31 +0700

    Add sessions and chatroom controller + view

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "pages#home"
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
 end
